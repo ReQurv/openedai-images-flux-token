df088449a82d8658f535ef4c27919f4de8265102
Tydron16 <114885071+ClaReQurv@users.noreply.github.com>
2025-07-26 09:46:56 +0200

    Update openedai.py

--- openedai.py
+++ openedai.py
@@ -12,10 +12,10 @@ API_KEY = os.getenv("API_KEY")  # Legge la chiave segreta dal file .env
 security = HTTPBearer()
 
 
-def validate_token(api_key_header: str = Security(security)):
+def validate_token(api_key_header = Security(security)):
     print("D", api_key_header)
     print("D", API_KEY)
-    if api_key_header != API_KEY:
+    if api_key_header.credentials != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid token")
     return True
 
